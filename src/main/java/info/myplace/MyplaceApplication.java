38dc03cb0b6c77e6a83327c65d650299abb96750
Jeongsu Yang <mycat83@gmail.com>
2021-04-15 22:28:06 +0900

    Initial commit

---
+++ src/main/java/info/myplace/MyplaceApplication.java
@@ -0,0 +1,13 @@
+package info.myplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/myplace/MyplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package info.myplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
