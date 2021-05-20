1dcc3174f35b995f592a1606bdf00b4a59aa5c6c
Jeongsu Yang <mycat83@gmail.com>
2021-05-20 22:29:08 +0900

    test: 태그 제거 문서화

--- place-api/src/test/java/info/myplace/placeapi/place/documentation/TagDocumentation.java
+++ place-api/src/test/java/info/myplace/placeapi/place/documentation/TagDocumentation.java
@@ -1,21 +1,24 @@
 package info.myplace.placeapi.place.documentation;
 
 import info.myplace.placeapi.Documentation;
-import info.myplace.placeapi.place.application.PlaceService;
 import info.myplace.placeapi.place.dto.PlaceResponse;
 import info.myplace.placeapi.place.dto.TagResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.request.ParameterDescriptor;
 
 import java.util.Arrays;
 
 import static info.myplace.placeapi.place.PlaceSteps.수리산_산림욕장;
 import static info.myplace.placeapi.place.PlaceSteps.태그_산림욕장;
+import static info.myplace.placeapi.place.PlaceSteps.태그_제거_요청;
 import static info.myplace.placeapi.place.PlaceSteps.태그_추가_요청;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 
 @DisplayName("태그 관리 문서화")
 class TagDocumentation extends Documentation {
@@ -35,9 +38,6 @@ class TagDocumentation extends Documentation {
             ))
             .build();
 
-    @MockBean
-    private PlaceService placeService;
-
     @Test
     @DisplayName("태그를 추가한다")
     void addTag() {
@@ -49,6 +49,26 @@ class TagDocumentation extends Documentation {
         // when
         태그_추가_요청(
                 given("tag/add", requestFields(requestFieldDescriptors)),
+                수리산_산림욕장_응답,
+                태그_산림욕장
+        );
+    }
+
+    @Test
+    @DisplayName("태그를 제거한다")
+    void removeTag() {
+        // given
+        ParameterDescriptor[] pathParameterDescriptors = {
+                parameterWithName("placeId").description("장소 ID")
+        };
+
+        ParameterDescriptor[] requestParameterDescriptors = {
+                parameterWithName("tagName").description("태그명")
+        };
+
+        // when
+        태그_제거_요청(
+                given("tag/remove", pathParameters(pathParameterDescriptors), requestParameters(requestParameterDescriptors)),
                 수리산_산림욕장_응답,
                 태그_산림욕장
         );
